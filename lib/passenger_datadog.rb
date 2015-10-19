8f8f7618ce95149c9cdc02b50248eb01474c2ed4
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-10-19 16:39:31 -0400

    Reinitialize statsd on every run

--- lib/passenger_datadog.rb
+++ lib/passenger_datadog.rb
@@ -5,37 +5,36 @@ class PassengerDatadog
   GROUP_STATS = %W(capacity_used processes_being_spawned enabled_process_count disabling_process_count disabled_process_count).freeze
   PROCESS_STATS = %w(processed sessions busyness concurrency cpu rss private_dirty pss swap real_memory vmsize uptime).freeze
 
-  def initialize
-    @statsd = Statsd.new
-  end
+  class << self
+    def run
+      @statsd = Statsd.new
+      #status = `sudo passenger-status --show=xml`
+      status = `passenger-status --show=xml`
+      return if status.empty?
 
-  def status
-    #status = `sudo passenger-status --show=xml`
-    status = `passenger-status --show=xml`
-    return if status.empty?
+      # Good job Passenger 4.0.10. Return non xml in your xml output.
+      status = status.split("\n")[3..-1].join("\n") unless status.start_with?('<?xml')
+      parsed = Nokogiri::XML(status)
 
-    # Good job Passenger 4.0.10. Return non xml in your xml output.
-    status = status.split("\n")[3..-1].join("\n") unless status.start_with?('<?xml')
-    parsed = Nokogiri::XML(status)
+      pool_used = parsed.xpath('//process_count').text
+      @statsd.gauge('passenger.pool.used', pool_used)
 
-    pool_used = parsed.xpath('//process_count').text
-    @statsd.gauge('passenger.pool.used', pool_used)
+      pool_max = parsed.xpath('//max').text
+      @statsd.gauge('passenger.pool.max', pool_max)
 
-    pool_max = parsed.xpath('//max').text
-    @statsd.gauge('passenger.pool.max', pool_max)
+      request_queue = parsed.xpath('//supergroups/supergroup/group/get_wait_list_size').text
+      @statsd.gauge('passenger.request_queue', request_queue)
 
-    request_queue = parsed.xpath('//supergroups/supergroup/group/get_wait_list_size').text
-    @statsd.gauge('passenger.request_queue', request_queue)
-
-    parsed.xpath('//supergroups/supergroup/group').each do |group|
-      GROUP_STATS.each do |stat|
-        @statsd.gauge("passenger.#{stat}", group.xpath(stat).text)
+      parsed.xpath('//supergroups/supergroup/group').each do |group|
+        GROUP_STATS.each do |stat|
+          @statsd.gauge("passenger.#{stat}", group.xpath(stat).text)
+        end
       end
-    end
 
-    parsed.xpath('//supergroups/supergroup/group/processes/process').each_with_index do |process, index|
-      PROCESS_STATS.each do |stat|
-        @statsd.gauge("passenger.#{stat}", process.xpath(stat).text, tags: ["passenger-process:#{index}"])
+      parsed.xpath('//supergroups/supergroup/group/processes/process').each_with_index do |process, index|
+        PROCESS_STATS.each do |stat|
+          @statsd.gauge("passenger.#{stat}", process.xpath(stat).text, tags: ["passenger-process:#{index}"])
+        end
       end
     end
   end
