a76e31f9a1d01366864f95a8d59952fb79966481
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-10-22 21:48:57 -0400

    Enforce the use of expect in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect # Disable `should`
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
@@ -25,6 +26,7 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
+    mocks.syntax = :expect # Disable `should_receive` and `stub`
   end
 
   # The settings below are suggested to provide a good initial experience
